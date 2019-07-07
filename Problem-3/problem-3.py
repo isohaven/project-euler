7839b618302aea3e108cd8c203eebe29331a7132
isohaven <ridgehaven320@gmail.com>
2019-07-07 15:22:19 -0400

    solved prob 3 with trial division

--- Problem-3/problem-3.py
+++ Problem-3/problem-3.py
@@ -1,7 +1,30 @@
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-# find fast factorization error
+# find fast factorization algo 
+# current is just trial division
 def prime_factors(n):
-    pass
+    primes = []
+    f = 3
+    # remove factors of 2
+    while n % 2 == 0:
+        primes.append(2)
+        n = n//2
+    # init for odd factors
+    if n == 1:
+        return primes
+    else:
+        f = 3
+    # end if prime
+    while(True):
+        if n < f*f:
+            primes.append(n)
+            return primes
+        else:
+            # trial division
+            q = n//f
+            r = n % f
+            # loop on odd ints
+            if r > 0:
+                f += 2
+            else:
+                primes.append(f)
+                n = q
+print(max(prime_factors(600851475143))) #6857
